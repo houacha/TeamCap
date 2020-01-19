6578506738bd2bdbef2821a65bf65e6e78bb989c
Shalini Shrinate <you@example.com>
2020-01-19 11:58:59 -0600

    added properties to Model Couple.

--- WeddingPlanner/Models/Couple.cs
+++ WeddingPlanner/Models/Couple.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,43 @@ namespace WeddingPlanner.Models
 {
     public class Couple
     {
+
+        [Key]
+
+        public int Id { get; set; }
+
+        [Display(Name = "Partner First Name")]
+        public string Partner1FirstName { get; set; }
+
+        [Display(Name = "Partner Last Name")]
+        public string Partner1LastName { get; set; }
+
+        [Display(Name = "Partner First Name")]
+        public string Partner2FirstName { get; set; }
+
+        [Display(Name = "Partner Last Name")]
+        public string Partner2LastName { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string CoupleStreetAddress { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "Zipcode")]
+        public int Zipcode { get; set; }
+
+        [Display(Name = "Wedding Budget")]
+        public double WeddingBudget { get; set; }
+
+        [Display(Name = "Estimated Total Cost")]
+        public double EstimatedTotal { get; set; }
+
+        [Display(Name = "Email")]
+        public string CoupleEmail { get; set; }
+
+        [Display(Name = "Phone")]
+        public string CouplePhone { get; set; }
+
     }
 }
