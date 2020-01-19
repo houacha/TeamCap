6abddf70004f39621364e98bf4d2c6c55c2dc2a8
Shalini Shrinate <you@example.com>
2020-01-19 12:16:35 -0600

    updated Model Celebrant with properties.

--- WeddingPlanner/Models/Celebrant.cs
+++ WeddingPlanner/Models/Celebrant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,31 @@ namespace WeddingPlanner.Models
 {
     public class Celebrant
     {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Display(Name = "Celebrant's Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Celebrant Email")]
+        public string CelebrantEmail { get; set; }
+
+        [Display(Name = "Celebrant Phone")]
+        public int CelebrantPhone { get; set; }
+
+        [Display(Name = "Ready to travel")]
+        public bool DoesTravel { get; set; }
+
+        [Display(Name = "Religious services offered")]
+        public string ReligionsServed { get; set; }
+
+        [Display(Name = "Do they sevice Co-habitants")]
+        public bool ServicesCohabitants { get; set; }
+
+        [Display(Name = "LGBTQFriendly")]
+        public bool LGBTQFriendly { get; set; }
+
+       
     }
 }
