faf243e8ba310ef57a3339bbd94c666d1fcdc2d2
Shalini Shrinate <you@example.com>
2020-01-19 12:09:43 -0600

    updated Model Venue with properties.

--- WeddingPlanner/Models/Venue.cs
+++ WeddingPlanner/Models/Venue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,53 @@ namespace WeddingPlanner.Models
 {
     public class Venue
     {
+        [Key]
+
+        [Display(Name = "Venue Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Venue Email")]
+        public string VenueEmail { get; set; }
+
+        [Display(Name = "Venue Phone")]
+        public string VenuePhone { get; set; }
+
+        [Display(Name = "LGBTQ friendly")]
+        public bool LGBTQFriendly { get; set; }
+
+        [Display(Name = "Kid-friendly")]
+        public bool KidFriendly { get; set; }
+
+        [Display(Name = "Pet-Friendly")]
+        public bool PetFriendly { get; set; }
+
+        [Display(Name = "Wheelchair Accessible")]
+        public bool HandicapAccessible { get; set; }
+
+        [Display(Name = "Religions served")]
+        public string ReligionsServed { get; set; }
+
+        [Display(Name = "Does it serve CoHabitants")]
+        public bool ServesCohabitants { get; set; }
+
+        [Display(Name = "Services Offered")]
+        public string serviceOffered { get; set; }
+        /*ceremony, reception, both*/
+
+        [Display(Name = "Does it provide Lodging")]
+        public bool ProvidesLodging { get; set; }
+
+        [Display(Name = "Does it allow third-party decor")]
+        public bool AllowsDecor { get; set; }
+
+        [Display(Name = "Does it allow third-party Celebrant")]
+        public bool ThirdPartyCelebrant { get; set; }
+
+        [Display(Name = "Does it allow third-party Catering")]
+        public bool ThirdPartyCatering { get; set; }
+
+        [Display(Name = "Does it allow third-party DJ")]
+        public bool ThirdPartyDJ { get; set; }
+
     }
 }
