0f95d2c85b84b9c5fa401a3997c597ceeec124b9
Shalini Shrinate <you@example.com>
2020-01-19 15:55:15 -0600

    Added and updated Model VendorPackage with properties, not yet added FK.

--- WeddingPlanner/Models/VendorPackage.cs
+++ WeddingPlanner/Models/VendorPackage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,14 @@ namespace WeddingPlanner.Models
 {
     public class VendorPackage
     {
+        [Key]
+        public int PackageID { get; set; }
+
+        [Display(Name = "VEndor Package Description")]
+        public string PackageDescription { get; set; }
+
+        [Display(Name = "Package Price")]
+        public double PackagePrice { get; set; }
+
     }
 }
