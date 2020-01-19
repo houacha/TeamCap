772b1063dbd91752de267c414b03a94678140f44
Shalini Shrinate <you@example.com>
2020-01-19 12:23:27 -0600

    updated Model Caterer with properties.

--- WeddingPlanner/Models/Caterer.cs
+++ WeddingPlanner/Models/Caterer.cs
@@ -7,5 +7,36 @@ namespace WeddingPlanner.Models
 {
     public class Caterer
     {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Display(Name = "Caterer Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Caterer Email")]
+        public string CatererEmail { get; set; }
+
+        [Display(Name = "Caterer Phone")]
+        public string CatererPhone { get; set; }
+
+        [Display(Name = "LGBTQFriendly")]
+        public bool LGBTQfriendly { get; set; }
+
+        [Display(Name = "Cuisine types offered")]
+        public string CuisineTypes { get; set; }
+
+        [Display(Name = "VeganFriendly")]
+        public bool ServesVegan { get; set; }
+
+        [Display(Name = "Food Allergy Options catered")]
+        public string FoodAllergyOptions { get; set; }
+
+        [Display(Name = "Per Guest Estimate Low")]
+        public double PerGuestEstimateLow { get; set; }
+
+        [Display(Name = "Per Guest Estimated High")]
+        public double PerGuestEstimateHigh { get; set; }
+
     }
 }
