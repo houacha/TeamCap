ce135e146bf99689642dc9f92c6dbc1f6047ba83
Shalini Shrinate <you@example.com>
2020-01-19 16:00:49 -0600

    Added and updated Model DerviceContract with properties, yet to add priceconfirmation and FK.

--- WeddingPlanner/Models/ServiceContract.cs
+++ WeddingPlanner/Models/ServiceContract.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace WeddingPlanner.Models
 {
     public class ServiceContract
     {
+        [Key]
+        public int ContractId { get; set; }
+
+        [Display(Name = "Contract Description")]
+        public string ContractDescription { get; set; }
+
+        [Display(Name = "Contract Price")]
+        public string ContractPrice { get; set; }
     }
 }
