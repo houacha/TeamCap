05f2848f19aab9701dc62bb6d72359b269ce523c
Shalini Shrinate <you@example.com>
2020-01-19 02:08:53 -0600

    DJ Model with properties added.

---
+++ WeddingPlanner/Models/DJ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WeddingPlanner.Models
+{
+    public class DJ
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        [Display(Name = "DJ Email")]
+        public string DJEmail { get; set; }
+
+        [Display(Name = "DJ Phone")]
+        public string DJPhone { get; set; }
+
+        [Display(Name = "Music Genres Offered")]
+        public bool MusicGenresOffered { get; set; }
+
+        [Display(Name = "Ready To Travel")]
+        public bool DoesTravel { get; set; }
+
+        [Display(Name = "LGBTQFriendly")]
+        public bool LGBTQFriendly { get; set; }
+
+      
+    }
+}
