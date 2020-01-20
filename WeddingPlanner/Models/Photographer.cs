e769fef9d810fd780ef2c47c990f9d9664520d72
Newt Industries <info@newtindustries.com>
2020-01-20 14:48:10 -0600

    Completed seed except for couples, corrected models to include full addresses

--- WeddingPlanner/Models/Photographer.cs
+++ WeddingPlanner/Models/Photographer.cs
@@ -17,6 +17,17 @@ namespace WeddingPlanner.Models
         [Display(Name = "Photographer's Name")]
         public string Name { get; set; }
 
+        [Display(Name = "Street")]
+        public string Street { get; set; }
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Display(Name = "ZipCode")]
+        public string Zip { get; set; }
+        [Display(Name = "State")]
+        public string State { get; set; }
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
         [Display(Name = "Photographer's Email")]
         public string PhotographerEmail { get; set; }
 
