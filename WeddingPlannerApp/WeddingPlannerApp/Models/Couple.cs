eaa297a2bd39d3d7d2fd0e0f16b55e168fc8c165
houa cha <itshoua@gmail.com>
2020-01-21 09:34:11 -0600

    add views to vendor controller

--- WeddingPlannerApp/WeddingPlannerApp/Models/Couple.cs
+++ WeddingPlannerApp/WeddingPlannerApp/Models/Couple.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,5 +12,8 @@ namespace WeddingPlannerApp.Models
         [Key]
         public int Id { get; set; }
         public int CoupleId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
